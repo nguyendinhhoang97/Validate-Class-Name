2c775573eba60a7f0ca6cbb9fab5bc0a4e4d76d9
nguyendinhhoang97 <dinhhoangvcu@gmail.com>
2018-12-06 17:34:28 +0700

    upload

---
+++ src/ClassName.java
@@ -0,0 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ClassName {
+    private static final String NAME_REGEX ="^[ACP]\\d{4}[GHIKLM]$";
+
+    public ClassName(){
+
+    }
+
+    public boolean validate(String regex){
+        Pattern pattern = Pattern.compile(NAME_REGEX);
+        Matcher matcher = pattern.matcher(regex);
+
+        return matcher.matches();
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,21 @@
-public class Test {
-    private static ValidateNameOfClass validateNameOfClass;
-    public static final String[] validName = new String[] { "C0318G" };
-    public static final String[] invalidName = new String[] { "M0318G" };
+class Test {
+    private static ClassName exampleClassName;
+    private static final String[] valiClassName = new String[] {"C0318G"};
+    private static final String[] invaliClassName = new String[] { "M0318G", "P0323A"};
 
-    public static void main(String args[]) {
-        validateNameOfClass = new ValidateNameOfClass();
-        for (String email : validName) {
-            boolean isvalid = validateNameOfClass.validate(email);
-            System.out.println("Email is " + email +" is valid: "+ isvalid);
+    public static void main(String[] args) {
+
+        exampleClassName = new ClassName();
+
+        for (String className:valiClassName) {
+            boolean isValid = exampleClassName.validate(className);
+            System.out.println("Class name is " + className +" is valid: "+ isValid);
         }
-        for (String email : invalidName) {
-            boolean isvalid = validateNameOfClass.validate(email);
-            System.out.println("Email is " + email +" is valid: "+ isvalid);
+
+        for (String className:invaliClassName) {
+            boolean isValid = exampleClassName.validate(className);
+            System.out.println("Class name is " + className + " is valid: " + isValid);
         }
+
     }
 }
